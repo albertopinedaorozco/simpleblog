d280f25834b9ede41aedb4a64197d17f571d4cfb
Fireman <ing.fireman@gmail.com>
2019-01-10 15:53:34 -0500

    archivos de configuracion heroku y actualizaciones

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+ukmg(4(i8nnxg#4q#d$gvx4mv27qnkn9k5(w7bm!-4ij7*i$p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
